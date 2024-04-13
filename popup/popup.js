ba4f47c755ba99f94b8fb0c26e73f066f81ed6ed
ecarnovsky <ecarnovsky@gmail.com>
2024-04-12 21:39:34 -0400

    Total number of seconds in the playlist can now be calculated

--- popup/popup.js
+++ popup/popup.js
@@ -2,8 +2,38 @@
 window.onload = start
 
 async function start(){
-    console.log(await getBodyHTML())
+
+    let bodyHTML = await getBodyHTML()
+
+    let parser = new DOMParser()
+    let doc = parser.parseFromString(bodyHTML, "text/html")
+    let videos = doc.querySelectorAll('ytd-playlist-panel-video-renderer')
+
+
+   let totalVideoSeconds = 0
+
+    videos.forEach((element) => {
+        let timeStr = element.querySelector('ytd-thumbnail-overlay-time-status-renderer').innerText
+
+        let timeArr = timeStr.split('\n')[0].split(':')
+
+        for(let i = 0; i < timeArr.length; i++){
+
+            if(i === 0){
+                totalVideoSeconds += Number(timeArr[timeArr.length - 1 - i])
+            } else if (i === 1){
+                totalVideoSeconds += Number(timeArr[timeArr.length - 1 - i]) * 60
+            } else {
+                totalVideoSeconds += Number(timeArr[timeArr.length - 1 - i]) * 3600
+            }
+        }
+    })
+
+    console.log("Total seconds: " + totalVideoSeconds)
+    
 }
+
+
 
 function getBodyHTML(){
 
